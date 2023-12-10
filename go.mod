1fc601f2aab7d3cb0d3b12fada0c76a02269dc6c
Andres <andres.costa@gmail.com>
2023-12-09 19:59:14 -0500

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/andrescosta/jobicolet-sdk-go
+
+go 1.21.4
---
+++ pkg/sdk/sdk.go
@@ -0,0 +1,68 @@
+package sdk
+
+// #include <stdlib.h>
+import "C"
+
+import (
+	"runtime"
+	"unsafe"
+)
+
+type EventFunc func(string) (uint64, string)
+
+var OnEvent EventFunc
+
+type Level uint32
+
+const (
+	DebugLevel Level = iota
+	InfoLevel
+	WarnLevel
+	ErrorLevel
+	FatalLevel
+	PanicLevel
+	NoLevel
+)
+
+const (
+	NoError uint64 = 0
+)
+
+func Log(level Level, message string) {
+	ptr, size := StringToPtr(message)
+	_log(level, ptr, size)
+	runtime.KeepAlive(message) // keep message alive until ptr is no longer needed.
+}
+
+//go:wasmimport env log
+func _log(level Level, ptr, size uint32)
+
+func PtrToString(ptr uint32, size uint32) string {
+	return unsafe.String((*byte)(unsafe.Pointer(uintptr(ptr))), size)
+}
+
+func StringToPtr(s string) (uint32, uint32) {
+	ptr := unsafe.Pointer(unsafe.StringData(s))
+	return uint32(uintptr(ptr)), uint32(len(s))
+}
+
+func StringToLeakedPtr(s string) (uint32, uint32) {
+	size := C.ulong(len(s))
+	ptr := unsafe.Pointer(C.malloc(size))
+	copy(unsafe.Slice((*byte)(ptr), size), s)
+	return uint32(uintptr(ptr)), uint32(size)
+}
+
+//export ver
+func ver() uint32 {
+	return 0
+}
+
+//export event
+func event(ptr, size uint32) (uint64, uint64) {
+	data := PtrToString(ptr, size)
+	code, result := OnEvent(data)
+	ptr, size = StringToLeakedPtr(result)
+	ptrRes := (uint64(ptr) << uint64(32)) | uint64(size)
+	return code, ptrRes
+}
