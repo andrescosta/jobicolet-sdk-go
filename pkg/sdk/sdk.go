7e5880c1bf1a294e914db67800a12eed6b385392
Andres <andres.costa@gmail.com>
2024-01-03 11:48:49 -0500

    refactors

--- pkg/sdk/sdk.go
+++ pkg/sdk/sdk.go
@@ -8,7 +8,7 @@ import (
 	"unsafe"
 )
 
-type EventFunc func(string) (uint64, string)
+type EventFunc func(uint32, string) (uint64, string)
 
 var OnEvent EventFunc
 
@@ -28,14 +28,14 @@ const (
 	NoError uint64 = 0
 )
 
-func Log(level Level, message string) {
+func Log(id uint32, level Level, message string) {
 	ptr, size := StringToPtr(message)
-	_log(level, ptr, size)
+	_log(id, level, ptr, size)
 	runtime.KeepAlive(message) // keep message alive until ptr is no longer needed.
 }
 
 //go:wasmimport env log
-func _log(level Level, ptr, size uint32)
+func _log(id uint32, level Level, ptr, size uint32)
 
 func PtrToString(ptr uint32, size uint32) string {
 	return unsafe.String((*byte)(unsafe.Pointer(uintptr(ptr))), size)
@@ -59,9 +59,9 @@ func ver() uint32 {
 }
 
 //export event
-func event(ptr, size uint32) (uint64, uint64) {
+func event(id, ptr, size uint32) (uint64, uint64) {
 	data := PtrToString(ptr, size)
-	code, result := OnEvent(data)
+	code, result := OnEvent(id, data)
 	ptr, size = StringToLeakedPtr(result)
 	ptrRes := (uint64(ptr) << uint64(32)) | uint64(size)
 	return code, ptrRes
